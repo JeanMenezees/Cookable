bef5139f5ec62792259b41107971836d8b4b4d21
Jean Menezes <jeanvbonimenezes@gmail.com>
2022-06-13 21:23:45 -0300

    chore: configurar dependencias iniciais do projeto

---
+++ src/common/components/button/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+
+export default function Button(): JSX.Element {
+    return <TouchableOpacity>
+        <Text>Button</Text>
+    </TouchableOpacity>
+}
