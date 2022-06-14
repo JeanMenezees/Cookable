a0d0838e9912e229965cc697176fae723c32a54c
Jean Menezes <jeanvbonimenezes@gmail.com>
2022-06-13 22:39:12 -0300

    style: aplicar lint

--- src/common/components/text/index.tsx
+++ src/common/components/text/index.tsx
@@ -3,13 +3,13 @@ import { StyleSheet, Text } from 'react-native';
 import { SoraTextProps } from './props';
 
 export default function SoraText(props: SoraTextProps): JSX.Element { 
-    return <Text style={[...props.extraStyle, styles.text]}>
-        {props.children}
-    </Text>
+	return <Text style={[...props.extraStyle, styles.text]}>
+		{props.children}
+	</Text>;
 }
 
 const styles = StyleSheet.create({
-    text: {
-        fontFamily: "Sora_400Regular"
-    }
+	text: {
+		fontFamily: 'Sora_400Regular'
+	}
 });
