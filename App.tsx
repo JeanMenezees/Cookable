65afad2cfb614f8225eee5360cbf06a8216c7f43
Jean Menezes <jeanvbonimenezes@gmail.com>
2022-06-13 21:32:38 -0300

    feat: carregar fontes do app com expo google fonts

--- App.tsx
+++ App.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Sora_400Regular, useFonts } from '@expo-google-fonts/sora';
+import AppLoading from 'expo-app-loading';
+
 export default function App() {
-	return (
+	let [fontsLoaded] = useFonts({
+		Sora_400Regular
+	});
+
+	return fontsLoaded ? (
 		<View style={styles.container}>
 			<Text>Open up App.tsx to start working on your app!</Text>
 			<StatusBar style="auto" />
 		</View>
-	);
+	) : <AppLoading />;
 }
 
 const styles = StyleSheet.create({
