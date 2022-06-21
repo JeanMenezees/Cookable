73b32f2c362bb251ff853f5a76ce5708db981844
Jean Menezes <jeanvbonimenezes@gmail.com>
2022-06-20 23:14:28 -0300

    style: aplicar lint no projeto

--- src/screens/recipies/index.tsx
+++ src/screens/recipies/index.tsx
@@ -1,46 +1,46 @@
-import React from "react";
-import { FlatList, StyleSheet } from "react-native";
-import SoraText from "../../common/components/text";
-import TemplateScreen from "../../common/screens/template-screen";
-import Recipie from "./components/recipie";
+import React from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+import SoraText from '../../common/components/text';
+import TemplateScreen from '../../common/screens/template-screen';
+import Recipie from './components/recipie';
 
 const recipies = [
-  {
-    titulo: "Bolo de cenoura",
-    description: "com cobertura de chocolate...",
-  },
-  {
-    titulo: "Bolo de chocolate",
-    description: "com cobertura de morango...",
-  },
-  {
-    titulo: "Bolo de morango",
-    description: "com cobertura de chocolate...",
-  },
+	{
+		titulo: 'Bolo de cenoura',
+		description: 'com cobertura de chocolate...',
+	},
+	{
+		titulo: 'Bolo de chocolate',
+		description: 'com cobertura de morango...',
+	},
+	{
+		titulo: 'Bolo de morango',
+		description: 'com cobertura de chocolate...',
+	},
 ];
 
 export default function Recipies(): JSX.Element {
-  return (
-    <TemplateScreen>
-      <SoraText extraStyle={styles.titulo}>Receitas</SoraText>
-      <FlatList
-        data={recipies}
-        renderItem={({ item }) => (
-          <Recipie
-            title={item.titulo}
-            description={item.description}
-          />
-        )}
-        keyExtractor={(item) => item.titulo}
-      />
-    </TemplateScreen>
-  );
+	return (
+		<TemplateScreen>
+			<SoraText extraStyle={styles.titulo}>Receitas</SoraText>
+			<FlatList
+				data={recipies}
+				renderItem={({ item }) => (
+					<Recipie
+						title={item.titulo}
+						description={item.description}
+					/>
+				)}
+				keyExtractor={(item) => item.titulo}
+			/>
+		</TemplateScreen>
+	);
 }
 
 const styles = StyleSheet.create({
-  titulo: {
-    fontSize: 32,
-    marginVertical: 16,
-    paddingHorizontal: 16,
-  },
+	titulo: {
+		fontSize: 32,
+		marginVertical: 16,
+		paddingHorizontal: 16,
+	},
 });
